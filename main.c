30dcdc41297d0bd4122b9e6e367554efecea633a
jayghevariya <75292433+jayghevariya@users.noreply.github.com>
2021-12-01 14:51:53 +0530

    Update main.c

--- main.c
+++ main.c
@@ -13,26 +13,27 @@ void delay(int number_of_seconds)
 
 int main()
 {
-    travel(4);
 
-    // MinimumACnoStart=(MinimumACno*)malloc(sizeof(MinimumACno));        
-    // MinimumACnoEnd=(MinimumACno*)malloc(sizeof(MinimumACno));            
-    // MinimumACnoPoint=(MinimumACno*)malloc(sizeof(MinimumACno));
-    // MinimumACnoEnd=NULL;
-    // MinimumACnoPoint=NULL;
-    // MinimumACnoStart=NULL;
+	
 
-    // startPtr=(Account*)malloc(sizeof(Account)*2);
-    // MaxLen=2;
-    // Point=0;
-    // Last=-1;
-	// int First;
-    // while (1)
-    // {
-    //     mainprompt();           //Printing Main Prompt On screen
-    //     scanf("%d",&First);
-    //     firstselection(First);
-    // }
+    MinimumACnoStart=(MinimumACno*)malloc(sizeof(MinimumACno));        
+    MinimumACnoEnd=(MinimumACno*)malloc(sizeof(MinimumACno));            
+    MinimumACnoPoint=(MinimumACno*)malloc(sizeof(MinimumACno));
+    MinimumACnoEnd=NULL;
+    MinimumACnoPoint=NULL;
+    MinimumACnoStart=NULL;
+
+    startPtr=(Account*)malloc(sizeof(Account)*2);
+    MaxLen=2;
+    Point=0;
+    Last=-1;
+	int First;
+    while (1)
+    {
+        mainprompt();           //Printing Main Prompt On screen
+        scanf("%d",&First);
+        firstselection(First);
+    }
     
     
     return 0;
